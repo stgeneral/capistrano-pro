bd6ff37a8f116db71a41b2894336c1890840b10b
Dmitry Babenko <dmitry@babenko.in.ua>
2012-04-18 13:38:00 +0300

    Fix path to unicorn tasks

--- lib/capistrano-pro/recipes/deploy/unicorn.rb
+++ lib/capistrano-pro/recipes/deploy/unicorn.rb
@@ -1,18 +1,17 @@
-require 'capistrano-pro/ext/unicorn'
+require 'capistrano-pro/unicorn'
 
 Capistrano::Configuration.instance.load do
   namespace :deploy do
     desc 'Start Unicorn (alias to unicorn:start)'
     task :start do
       unicorn.start
-    end 
-    
-    
+    end
+
     desc 'Stop Unicorn (alias to unicorn:stop)'
     task :stop do
       unicorn.stop
     end
-    
+
     desc 'Restart Unicorn (alias to unicorn:restart)'
     task :restart do
       unicorn.restart
