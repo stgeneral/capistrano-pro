541f4b4961c47119a3f2ad06643b077c0ec0d673
Dmitry Babenko <dmitry@babenko.in.ua>
2012-04-18 13:10:48 +0300

    Initial commit

---
+++ lib/capistrano-pro.rb
@@ -0,0 +1,7 @@
+require "capistrano-pro/version"
+
+module Capistrano
+  module Pro
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano-pro/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Pro
+    VERSION = "0.0.1"
+  end
+end
