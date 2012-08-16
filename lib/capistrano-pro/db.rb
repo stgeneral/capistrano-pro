32c4b4894679758827d92cf3d69dc8dbddc3bc7c
Dmitry Babenko <dmitry@babenko.in.ua>
2012-08-16 16:02:31 +0300

    DB deploy configs with rails_env suffix

--- lib/capistrano-pro/db.rb
+++ lib/capistrano-pro/db.rb
@@ -1,9 +1,9 @@
 Capistrano::Configuration.instance.load do
   namespace :db do
-    desc "Uploads ./config/database-production.yml to :shared_path/config/database.yml"
+    desc "Uploads ./config/database-#{rails_env}.yml to :shared_path/config/database.yml"
     task :upload_config, :except => { :no_release => true }, :role => :app do
       run "mkdir -p #{shared_path}/config";
-      upload("./config/database-production.yml", "#{shared_path}/config/database.yml")
+      upload("./config/database-#{rails_env}.yml", "#{shared_path}/config/database.yml")
     end
   
     desc "Create symlink :release_path/config/database.yml to :shared_path/config/database.yml"
